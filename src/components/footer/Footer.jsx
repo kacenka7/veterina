711b94409bae1696f089b181445f3052a35ad644
kacenka7 <157618583+kacenka7@users.noreply.github.com>
2024-10-09 22:06:56 +0200

    verze 1.02

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,18 +5,21 @@ export const Footer = () => {
     return (
         <footer className="footer">
 
-        <div className="footer__info">
-                <div className="footer__box">
-                    <span className="material-symbols-outlined footer__icon">call</span>
+
+            <div className="footer__container">
+                <span className="material-symbols-outlined footer__icon">call</span>
+                    <div className="footer__box">
                     <p className="footer__data">+420 789 456 123</p>
                 </div>
+            </div>
+            <div className="footer__container">
+                <span className="material-symbols-outlined footer__icon">schedule</span>
                 <div className="footer__box">
-                    <span className="material-symbols-outlined footer__icon">schedule</span>
                     <p className="footer__data">Po - Pá: 10:00 - 18:00</p>
                     <p className="footer__data">So:  9:00-11:00</p>
                 </div>
+            </div>
 
-        </div>
 
             <p className="footer__sign"> © Kate Lenc 2024</p>
         </footer>
--- src/components/sluzby/Sluzby.jsx
+++ src/components/sluzby/Sluzby.jsx
@@ -12,32 +12,32 @@ export const Sluzby = () => {
             <div className="sluzby__container">
 
                 <div className="sluzby__item">
-                    <div className="sluzby__box"> <h3>Interní medicína</h3> </div>
+                    <div className="sluzby__box"> <h4>Interní medicína</h4> </div>
                     <span className="material-symbols-outlined sluzby__icon">stethoscope</span>
                 </div>
 
                 <div className="sluzby__item">
-                    <div className="sluzby__box"> <h3>Kardiologie</h3> </div>
+                    <div className="sluzby__box"> <h4>Kardiologie</h4> </div>
                     <span className="material-symbols-outlined sluzby__icon">ecg_heart</span>
                 </div>
 
                 <div className="sluzby__item">
-                    <div className="sluzby__box"> <h3>Stomatologie</h3> </div>
+                    <div className="sluzby__box"> <h4>Stomatologie</h4> </div>
                     <span className="material-symbols-outlined sluzby__icon">dentistry</span>
                 </div>
 
                 <div className="sluzby__item">
-                    <div className="sluzby__box"> <h3>Chirurgie</h3> </div>
+                    <div className="sluzby__box"> <h4>Chirurgie</h4> </div>
                     <span className="material-symbols-outlined sluzby__icon">surgical</span>
                 </div>
 
                 <div className="sluzby__item">
-                    <div className="sluzby__box"> <h3>Ortopedie</h3> </div>
+                    <div className="sluzby__box"> <h4>Ortopedie</h4> </div>
                     <span className="material-symbols-outlined sluzby__icon">tibia_alt</span>
                 </div>
 
                 <div className="sluzby__item">
-                    <div className="sluzby__box"> <h3>Prevence</h3> </div>
+                    <div className="sluzby__box"> <h4>Prevence</h4> </div>
                     <span className="material-symbols-outlined sluzby__icon">prescriptions</span>
                 </div>
 
